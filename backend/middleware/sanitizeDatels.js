dd8ce9d79105feab1af1f6dab19714760fc4c42c
Sagor20005 <ms6392883@gmail.com>
2025-07-02 06:23:29 +0600

    fix length error in python res

--- backend/middleware/sanitizeDatels.js
+++ backend/middleware/sanitizeDatels.js
@@ -15,8 +15,8 @@ function sanitizeDatels(req,resp,next){
       }
     })
     // Cheake Download field
-    if( req.body.Downloads.length < 1 ) emptyFields.push("Downloads")
-    if( req.body.Images.length < 1) emptyFields.push("Images")
+    if( !req.body.Downloads ) emptyFields.push("Downloads")
+    if( !req.body.Images) emptyFields.push("Images")
     
     
     if(emptyFields.length > 0){
