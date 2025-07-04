f36dd27892736b458c737d753c237c1457554a89
Sagor20005 <ms6392883@gmail.com>
2025-07-04 16:32:15 +0600

    inharit build_trending

--- frontend/src/Components/ContentHome.js
+++ frontend/src/Components/ContentHome.js
@@ -3,44 +3,19 @@ import LoddingEffect from "./Assets/lodding1.gif"
 import errorEffect from "./Assets/error.gif"
 import {useEffect, useState} from "react"
 import { useNavigate } from "react-router-dom"
-import SeparateProductAreay from "../Utilities/separateProductAreay.js"
-import { useSelector, useDispatch } from 'react-redux'
-import { getMovies } from "../features/movies/moviesSlice"
-import movieIcon from "./Assets/movie.png"
 
-
-function ContentHome(){
+function ContentHome({ content_list, content_type, page }){
   const Navigate = useNavigate()
-  const dispatch = useDispatch()
   
-  // get data from redux storr
-  const Contents = useSelector((state)=> state.moviesList)
-
-  const { isLodding, isError, error, contents } = Contents
+  if(!page) page = 1
   
-  useEffect(()=>{
-    if(contents.length === 0) dispatch(getMovies())
-  },[])
-  
-  
-  // genarate html
-  const [contentHtml,setHtml] = useState(null)
-  function rendar(index){
-    if(isLodding){
-      setHtml(<img style={{width:"100%",height:"auto"}} className="lodding" src={LoddingEffect} alt="lodding"/>)
-    }else if(!isLodding && !isError && contents.length === 0){
-      setHtml(<img className="lodding" src={errorEffect} alt={error}/>)
-    } else if(!isLodding && !isError && contents){
-      let temp = <div className="content_list">
+  const contentHtml = <div className="content_list">
         {
-          SeparateProductAreay(contents,12)[index].map((content,i)=>{
+          content_list.slice( page*10-10, page*20 ).map((content,i)=>{
             return(
               <div key={i} onClick={()=>Navigate(`/content/${content.url_name}`,{state:content})} className="content">
                 <div className="image">
-                  <img src={content.Poster} onError={(e)=> {
-                    e.target.onerror = null;
-                    e.target.src = movieIcon
-                  }} alt={content.Title}/>
+                  <img src={content.Poster} alt={content.Title}/>
                 </div>
                 <div className="dtl">
                   <span>{ content.Title.length > 20 ? content.Title.slice(0,15) + ".." : content.Title }</span>
@@ -51,25 +26,7 @@ function ContentHome(){
           })
         }
       </div>
-      setHtml(temp)
-    }else if(!isLodding && isError){
-      setHtml(<img className="lodding" src={errorEffect} alt={error}/>)
-    }
-  }
-  
-  useEffect(()=>{
-    // function call to render
-    rendar(0)
-  },[contents,isLodding,isError])
-  
-  function handlePageination(e){
-    if(e.nodeName === "LI"){
-      rendar(e.innerText-1)
-    }
-  }
-    
-  
-  
+
   
   return(
     
@@ -77,23 +34,8 @@ function ContentHome(){
     
     <>
       <div className="content_containar" >
-        <span className="section_title">Latest Contants.</span>
+        <span className="section_title">Latest {content_type}.</span>
         {contentHtml}
-        {
-          
-          contents.length > 0 && <ul onClick={(e)=>handlePageination(e.target)} className="page_list">
-          {
-            (function(){
-              const elarr = []
-              for (let i = 1; i <= SeparateProductAreay(contents).length; i++){
-                elarr.push(<li key={i} >{i}</li>)
-              }
-              return elarr
-            })()
-          }
-        </ul>
-          
-        }
       </div>
     </>
     )
