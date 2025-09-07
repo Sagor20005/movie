543c71581b5778cd45f8aae6299c4a97f08215ef
Sagor20005 <ms6392883@gmail.com>
2025-09-07 12:15:59 +0600

    fix Mongoose url error

--- backend/database/mongoose.con.js
+++ backend/database/mongoose.con.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URL.endsWith("/") ? process.env.MONGODB_URL: `${process.env.MONGODB_URL}/`
+const mongooseUrl = process.env.MONGODB_URL
+const url = mongooseUrl ? mongooseUrl.endsWith("/") ? mongooseUrl : `${mongooseUrl}/` : null
 const name = "movieflex_Database"
 
   async function connectDb() {
     try {
       const conn = await mongoose.connect(url+name) 
       console.log('Database connected..')
-  }catch(e) {
-    console.log({
-      status: "Db connect faild.",
-      message: e.message
-    })
-  }
+    }catch(e) {
+      console.log({
+        status: "Db connect faild.",
+        message: e.message
+      })
+    }
 }
 module.exports = connectDb
